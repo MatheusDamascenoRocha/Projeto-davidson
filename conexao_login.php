3f4051e8c754125ad7cf728adb8aa1f29b9ea087
Matheus Damasceno <124222592+MatheusDamascenoRocha@users.noreply.github.com>
2023-05-17 10:30:24 -0300

    usando banco de dados up driver

--- conexao_login.php
+++ conexao_login.php
@@ -2,7 +2,7 @@
 	$host  ="localhost";
 	$user ="root";
 	$senha ="";
-	$bancodedados="oficina";
+	$bancodedados="up_driver";
                 
 	$mysqli = new mysqli($host,$user,$senha,$bancodedados);
 	if (empty($mysqli)) {
